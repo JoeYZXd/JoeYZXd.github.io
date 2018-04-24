c09b1f11912271ae611fbbfa0aca2644f2c408a5
zhangxiaodong <zhangxiaodong@yazuo.com>
2018-04-24 11:10:22 +0800

    Nikola auto commit.
    Nikola version: 7.8.14

--- conf.py
+++ conf.py
@@ -1364,5 +1364,3 @@ COMPILERS["orgmode"] = ('.org',)
 # Add org files to your POSTS, PAGES
 POSTS = POSTS + (("posts/*.org", "posts", "post.tmpl"),)
 PAGES = PAGES + (("stories/*.org", "stories", "story.tmpl"),)
-
-EDITOR = "emacs"
